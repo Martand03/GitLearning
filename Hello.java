8b69772251170bcbd3857fcddaea5a3304b1e682
Martand Mahajan <martandmahajan03@gmail.com>
2023-09-10 10:43:57 +0530

    commit new after login to account

--- Hello.java
+++ Hello.java
@@ -3,6 +3,8 @@ class Hello{
     public static void main (String ar[]){
         System.out.println("Hello");
         System.out.println("Bye");
+        System.out.println("Push from git bash");
+
     }
 
 }
